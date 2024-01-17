e64ff42f11e783e1f934013289814e745fbfc251
Nieves <smilenvs@gmail.com>
2024-01-17 10:11:09 +0100

    Puedo poner más cosas

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -1,24 +1,28 @@
-import {Router} from "express";
+import { Router } from "express";
 
 export const adminRouter = Router();
 
-export const productos: {title: string, imageUrl: string, description: string, price: string}[] = [];
+export const productos: { title: string, imageUrl: string, description: string, price: number }[] = [];
 
 adminRouter.get('/add-product', (req, res, next) => {
     console.log("Devolver formulario para meter productos");
     //res.send('<FORM action="/admin/product" method="POST"><input type="text" name="producto"> <button type="submit">Enviar</button></FORM>');
-    res.render('add-product', {pageTitle: "Formulario", path: "/admin/add-product"});
+    res.render('add-product', { pageTitle: "Formulario", path: "/admin/add-product" });
 });
 
-adminRouter.post('/add-product',(req, res, next) => {
+adminRouter.post('/add-product', (req, res, next) => {
     const title = req.body.title;
     const imageUrl = req.body.imageUrl;
     const description = req.body.description;
-    const price = req.body.price;
+    const price = +req.body.price; //El + es para que se transforme a número
 
-    if(req.body.title){
-        console.log('Ha llegado el siguiente producto',req.body.title);
-        productos.push({title: title, imageUrl: imageUrl, description: description, price: price});
+    if (req.body.title) {
+        console.log('Ha llegado el siguiente producto', req.body.title);
+        productos.push({ 
+            title: title, 
+            imageUrl: imageUrl, 
+            description: description, 
+            price: price });
     }
     res.redirect('/');
 });
